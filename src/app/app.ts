9a83014142fa68b55e3f6207a65c8b00ea1f73a4
Gabriel Nogueira <gabrielnogueiras@outlook.com>
2025-06-05 12:11:40 -0300

    Menu Title

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitle } from "./components/menu-title/menu-title";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuTitle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
