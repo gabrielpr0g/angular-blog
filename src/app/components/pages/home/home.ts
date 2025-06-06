3d7732cc83f2c7ee72a03c8601f7a10d3001bf6f
Gabriel Nogueira <gabrielnogueiras@outlook.com>
2025-06-06 12:23:56 -0300

    Desenhando small-cards

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MenuTitle } from "../../menu-title/menu-title";
 import { BigCard } from "../../big-card/big-card";
+import { SmallCard } from '../../small-card/small-card';
+import { MenuBar } from '../../menu-bar/menu-bar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuTitle, BigCard],
+  imports: [MenuTitle, BigCard, SmallCard],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
