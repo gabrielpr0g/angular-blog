160c889946507399fd91ab1fcccebfbf0e334995
Gabriel Nogueira <gabrielnogueiras@outlook.com>
2025-06-16 21:10:13 -0300

    Adicionando as paginas respectivas aos small cards

--- src/app/data/datafake.ts
+++ src/app/data/datafake.ts
@@ -15,13 +15,13 @@ export const dataFake = [
     "id":"3",
     "title": "Nova atualização Angular",
     "description": "Nova atualização Angular",
-    "photoCover":"https://vidafullstack.com.br/wp-content/uploads/2021/07/versao-12-angular-1080x675.png"
+    "photoCover":"https://external-preview.redd.it/qW5sXZSe_7w815bbdJh3mwhzBxyEdK13fFyRs3_8ZdQ.jpg?width=640&crop=smart&auto=webp&s=53d250c9db70f2b0fe7a93de0cf57e7f7ffc2a06"
   },
   {
     "id":"4",
     "title": "Nova atualização Angular",
     "description": "Nova atualização Angular",
-    "photoCover":"https://vidafullstack.com.br/wp-content/uploads/2021/07/versao-12-angular-1080x675.png"
+    "photoCover":"https://ionic.io/blog/wp-content/uploads/2024/02/angular-feature-image-1-1024x512.png"
   }
   
 ]
